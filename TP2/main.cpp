0b47dd778f1adcf741872f0d6672a3f3cc65a15f
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-10-11 18:36:42 +0200

    Encode/Decode JPEG are now 2 differents functions && reformat code

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 using namespace cimg_library;
 
-
+/*--------------- DCT ----------------*/
 CImg<double> applyDctOnBlock(CImg<unsigned char> block){
     // init vars
     int step = block.width(); // step = width or height of block
@@ -58,7 +58,9 @@ CImg<double> applyDctOnBlock(CImg<unsigned char> block){
 
     return dctBlock;
 }
+/*--------------- /DCT ----------------*/
 
+/*--------------- DCT-1 ----------------*/
 CImg<unsigned char> applyInvertedDctOnDCTBlock(CImg<double> dctBlock){
     // init vars
     int step = dctBlock.width(); // step = width or height of block
@@ -103,13 +105,11 @@ CImg<unsigned char> applyInvertedDctOnDCTBlock(CImg<double> dctBlock){
 
     return block;
 }
+/*--------------- /DCT-1 ----------------*/
 
-
+/*--------------- JPEGENCODER ----------------*/
 CImg<double> JPEGEncoder(CImg<unsigned char> image, float quality)
 {
-    CImg<unsigned char> compressed_image(image.width(),image.height(),1,1,0);
-    //compressed_image = image;
-
     // Quantization matrix
     CImg<> Q(8,8);
     Q(0,0)=16;   Q(0,1)=11;   Q(0,2)=10;   Q(0,3)=16;   Q(0,4)=24;   Q(0,5)=40;   Q(0,6)=51;   Q(0,7)=61;
@@ -123,9 +123,10 @@ CImg<double> JPEGEncoder(CImg<unsigned char> image, float quality)
     // Quality
     Q = Q * quality;
 
+	// init vars
+    CImg<double> compressed_image(image.width(),image.height(),1,1,0);
     int step = 8; // bloc 8*8
     CImg<unsigned char> image_block;
-    CImg<unsigned char> new_image_block;
     CImg<double> dct_block;
 
     // for each bloc in width    
@@ -141,6 +142,55 @@ CImg<double> JPEGEncoder(CImg<unsigned char> image, float quality)
             // TODO
             //CImg<double> quantifier
 
+
+
+            // affect bloc in final image
+            // foreach pixel in bloc width
+            for(int ii=0 ; ii<step ; ++ii) {
+                // foreach pixel in bloc height
+                for(int jj=0 ; jj<step ; ++jj){
+                    compressed_image(i*8+ii, j*8+jj)= dct_block(ii,jj);
+		            
+                }
+            }
+        }
+    }
+    return compressed_image;
+}
+/*--------------- /JPEGENCODER ----------------*/
+
+/*--------------- JPEGDECODER ----------------*/
+CImg<unsigned char> JPEGDecoder(CImg<double> image, float quality)
+{
+    // Quantization matrix
+    CImg<> Q(8,8);
+    Q(0,0)=16;   Q(0,1)=11;   Q(0,2)=10;   Q(0,3)=16;   Q(0,4)=24;   Q(0,5)=40;   Q(0,6)=51;   Q(0,7)=61;
+    Q(1,0)=12;   Q(1,1)=12;   Q(1,2)=14;   Q(1,3)=19;   Q(1,4)=26;   Q(1,5)=58;   Q(1,6)=60;   Q(1,7)=55;
+    Q(2,0)=14;   Q(2,1)=13;   Q(2,2)=16;   Q(2,3)=24;   Q(2,4)=40;   Q(2,5)=57;   Q(2,6)=69;   Q(2,7)=56;
+    Q(3,0)=14;   Q(3,1)=17;   Q(3,2)=22;   Q(3,3)=29;   Q(3,4)=51;   Q(3,5)=87;   Q(3,6)=80;   Q(3,7)=62;
+    Q(4,0)=18;   Q(4,1)=22;   Q(4,2)=37;   Q(4,3)=56;   Q(4,4)=68;   Q(4,5)=109;  Q(4,6)=103;  Q(4,7)=77;
+    Q(5,0)=24;   Q(5,1)=35;   Q(5,2)=55;   Q(5,3)=64;   Q(5,4)=81;   Q(5,5)=104;  Q(5,6)=113;  Q(5,7)=92;
+    Q(6,0)=49;   Q(6,1)=64;   Q(6,2)=78;   Q(6,3)=87;   Q(6,4)=103;  Q(6,5)=121;  Q(6,6)=120;  Q(6,7)=101;
+    Q(7,0)=72;   Q(7,1)=92;   Q(7,2)=95;   Q(7,3)=98;   Q(7,4)=112;  Q(7,5)=100;  Q(7,6)=103;  Q(7,7)=99;
+    // Quality
+    Q = Q * quality;
+
+	// init vars
+    CImg<unsigned char> uncompressed_image(image.width(),image.height(),1,1,0);
+    int step = 8; // bloc 8*8
+    CImg<unsigned char> new_image_block;
+    CImg<double> dct_block;
+
+    // for each bloc in width    
+    for(int i=0 ; i<image.width()/step ; ++i) {
+        // for each bloc in hight
+        for(int j=0 ; j<image.height()/step ; ++j) {
+            // get one bloc of 8*8 px
+            dct_block = image.get_crop( i*8, j*8, i*8+(step-1), j*8+(step-1));
+
+            // TODO
+            //CImg<double> quantifier INVERSE
+            
             // apply dct-1 on dctblock
             new_image_block = applyInvertedDctOnDCTBlock(dct_block);
 
@@ -149,15 +199,18 @@ CImg<double> JPEGEncoder(CImg<unsigned char> image, float quality)
             for(int ii=0 ; ii<step ; ++ii) {
                 // foreach pixel in bloc height
                 for(int jj=0 ; jj<step ; ++jj){
-                    compressed_image(i*8+ii, j*8+jj)= new_image_block(ii,jj);
+                    uncompressed_image(i*8+ii, j*8+jj)= new_image_block(ii,jj);
 		            
                 }
             }
         }
     }
-    return compressed_image;
+    return uncompressed_image;
 }
+/*--------------- /JPEGDECODER ----------------*/
 
+
+/*--------------- MAIN ----------------*/
 int main()
 {
     // Read the image "lena.bmp"
@@ -166,22 +219,31 @@ int main()
     // Take the luminance information 
     my_image.channel(0);
 
+	// Set quality
     float quality=1.; // low compression
     //float quality=30.; // strong compression
-    //CImg<unsigned char> comp_image = JPEGEncoder(my_image,quality);
-CImg<double> comp_image = JPEGEncoder(my_image,quality);
+    
+    // Compress to JPEG
+    CImg<double> compressed_image = JPEGEncoder(my_image,quality);
+    
+    // Uncompress
+    CImg<unsigned char> uncompressed_image = JPEGDecoder(compressed_image,quality);
 
-    // Display the bmp file
+    /* Display images */
+    // bmp file
     CImgDisplay main_disp(my_image,"Initial Image");
+    // compressed (by dct)
+    CImgDisplay comp_disp(compressed_image,"Compressed Image");
+    // Display the uncompressed file (by dct-1)
+    CImgDisplay uncomp_disp(uncompressed_image,"Uncompressed Image");
 
-    // Display the compressed file (by dct)
-    CImgDisplay comp_disp(comp_image,"Compressed Image");	
-
-    // Program end when windows are closed  
+	/* Wait */
     while (!main_disp.is_closed()); {
         main_disp.wait();
     }
 }
+/*--------------- /MAIN ----------------*/
+
 
 /* ---- explications ----- */
 
@@ -194,11 +256,6 @@ v
 j
 */
 
-// quantification
-//Q multiplier par le facteur de qualité
-
-
-// DCT : deux boucles for imbriquées (float ou double)
 
 
 
