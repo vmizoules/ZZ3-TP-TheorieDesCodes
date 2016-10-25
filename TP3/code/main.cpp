6793a8c71f6e6301fe76cc116b2a6393da333ec2
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-10-25 14:43:21 +0200

    Hamming: ok

--- TP3/code/main.cpp
+++ TP3/code/main.cpp
@@ -16,10 +16,10 @@
 #define DEBUG_RF true // Debug Information: Read File
 #define DEBUG_HE true // Debug Information: Hamming Encoding
 
-#define M1 inBuffer[3]
+#define M1 inBuffer[3] // bit de poids faible - LSB (droite)
 #define M2 inBuffer[2]
 #define M3 inBuffer[1]
-#define M4 inBuffer[0]
+#define M4 inBuffer[0] // bit de poids fort - MSB (gauche)
 
 using namespace std; 
 
@@ -86,15 +86,23 @@ vector<bitset<HAMMING_7> > HammingEncoding(vector<bitset<N> > bitsetVector)
 		// c : codage haming / G : matrice génératrice / M : message
 		// c = G(transposé) . M
 
-		// left bit
-		outBuffer[6] = M1 + M2 + M4;
-		outBuffer[5] = M1 + M3 + M4;
-		outBuffer[4] = M1;
-		outBuffer[3] = M2 + M3 + M4 ;
-		outBuffer[2] = M2;
-		outBuffer[1] = M3; // m3 = inBuffer[1]
-		outBuffer[0] = M4; // m4 = inBuffer[0]
-		// right bit		
+		// M4 bit de poid fort (gauche), M1 bit de poid faible (droite)
+
+		// XOR :
+		// 0 ^ 0 = 0
+		// 0 ^ 1 = 1
+		// 1 ^ 0 = 1
+		// 1 ^ 1 = 0
+
+		// right bit (poids faible - LSB)
+		outBuffer[0] = M1 ^ M2 ^ M4; //m1 + m2 + m4
+		outBuffer[1] = M1 ^ M3 ^ M4;
+		outBuffer[2] = M1;
+		outBuffer[3] = M2 ^ M3 ^ M4 ;
+		outBuffer[4] = M2;
+		outBuffer[5] = M3; // m3 = inBuffer[1]
+		outBuffer[6] = M4; // m4 = inBuffer[0]
+		// left bit (poids fort - MSB)
 
 		
 		if(DEBUG_HE) {
@@ -121,7 +129,7 @@ int main()
 	vector< bitset<HAMMING_7> > encode_data; 
 
 	// Read data to encode
-	input_data = readFile("test.txt");
+	input_data = readFile("test2.txt");
 
 	// Encode by Hamming (7,4) coding
 	encode_data = HammingEncoding(input_data);
