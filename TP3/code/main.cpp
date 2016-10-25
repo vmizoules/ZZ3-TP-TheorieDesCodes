e0e03a530c38b3c8910955597911d1fd79f5994e
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-10-25 17:16:10 +0200

    Add error injection

--- TP3/code/main.cpp
+++ TP3/code/main.cpp
@@ -3,6 +3,8 @@
 #include <string>         
 #include <bitset>        
 #include <vector>
+#include <stdlib.h>
+#include <time.h> 
 
 /**
  * Hamming (7,4)
@@ -10,12 +12,15 @@
 #define N 4
 #define HAMMING_7 7
 
+#define MAX_ERROR_PER_BITSET 1
+
 /**
  * Debug macros
  **/ 
 #define DEBUG_RF true // Debug Information: Read File
 #define DEBUG_HE true // Debug Information: Hamming Encoding
 #define DEBUG_HD true // Debug Information: Hamming Decoding
+#define DEBUG_EI true // Debug Information: Error Injection
 
 #define M1 inBuffer[0] // bit de poids faible - LSB (droite)
 #define M2 inBuffer[1]
@@ -30,7 +35,19 @@
 #define C6 inBuffer[5]
 #define C7 inBuffer[6] // bit de poids fort - MSB (gauche)
 
-using namespace std; 
+using namespace std;
+
+bool randInitialized = false;
+
+int getRand(int min, int max) {
+	if(! randInitialized) {
+		srand (time(NULL));
+		randInitialized = true;
+	}
+
+	return rand() % (max+1) + min;
+}
+
 
 /**
  * vector<bitset<N> > readFile(string filename)
@@ -45,7 +62,7 @@ vector<bitset<N> > readFile(string filename)
 	reader.open(filename.c_str(), ios::binary|ios::in);
 
 	if(DEBUG_RF) {
-		cout << "Read : \t";
+		cout << "Read :" << endl;
 	}
 
 	if(reader != NULL && reader.is_open())
@@ -83,7 +100,7 @@ vector<bitset<HAMMING_7> > HammingEncoding(vector<bitset<N> > bitsetVector)
 	vector<bitset<HAMMING_7> > encodedBitset;
 	
 	if(DEBUG_HE) {
-		std::cout << "Encode : \t";
+		std::cout << "Encode :" << endl;
 	}
 
 	for(vector<bitset<N> >::iterator i = bitsetVector.begin(); i != bitsetVector.end();++i)
@@ -127,12 +144,49 @@ vector<bitset<HAMMING_7> > HammingEncoding(vector<bitset<N> > bitsetVector)
 	return encodedBitset;
 }
 
-vector<bitset<HAMMING_7> > & injectError(vector<bitset<HAMMING_7> > & bitsetVector)
+vector<bitset<HAMMING_7> > injectError(vector<bitset<HAMMING_7> > & input)
 {
+	// init vars
+	int pos;
+	vector<bitset<HAMMING_7> > output;
 	
-	// TODO
+	if(DEBUG_EI) {
+		cout << endl << "-- Starting injection --" << endl;
+	}
+	
+	// for each bitset in the vector
+	for(vector<bitset<HAMMING_7> >::iterator i = input.begin(); i != input.end();++i) {
+		// get sample
+		bitset<HAMMING_7> buffer = *i;
 
-	return bitsetVector;
+		// there is between 0 and MAX_ERROR_PER_BITSET in a bitset
+		for(int i = 0 ; i < getRand(0,MAX_ERROR_PER_BITSET) ; ++i) {
+			// compute position
+			pos = getRand(0, HAMMING_7-1);
+			// inject error in position pos
+			buffer[pos] = buffer[pos] ^ 1;
+			
+			if(DEBUG_EI) {
+				cout << "Injecting error in position : " << pos+1 << endl;
+			}
+		}
+		
+		// add modified sample to output
+		output.push_back(buffer);
+	}
+	
+	if(DEBUG_EI) {
+		cout << "After injection :" << endl;
+	
+		for(vector<bitset<HAMMING_7> >::iterator i = output.begin(); i != output.end();++i) {
+			bitset<HAMMING_7> buff = *i;
+			cout << " | " << buff.to_string();
+		}
+	
+		cout << endl << "-- End injection --" << endl;
+	}
+	
+	return output;
 }
 
 int verifyBitset(bitset<HAMMING_7> inBuffer)
@@ -140,15 +194,11 @@ int verifyBitset(bitset<HAMMING_7> inBuffer)
 	bitset<3> position;
 	int pos = 0;
 	
-	// inject error in first bit
-	//inBuffer[0] = 0;
-	// TODO : REMOVE IT
-	
-	// right bit (poids faible - LSB)
-	position[0] = C4 ^ C5 ^ C6 ^ C7;
-	position[1] = C2 ^ C3 ^ C6 ^ C7;
-	position[2] = C1 ^ C3 ^ C5 ^ C7;
 	// left bit (poids fort - MSB)
+	position[2] = C4 ^ C5 ^ C6 ^ C7;
+	position[1] = C2 ^ C3 ^ C6 ^ C7;
+	position[0] = C1 ^ C3 ^ C5 ^ C7;
+	// right bit (poids faible - LSB)
 
 	// convert bitset to int
 	pos = (int) (position.to_ulong()) ;
@@ -161,7 +211,7 @@ vector<bitset<N> > HammingDecoding(vector<bitset<HAMMING_7> > & bitsetVector)
 	vector<bitset<N> > decodedBitset;
 	
 	if(DEBUG_HD) {
-		std::cout << "Decode : \t";
+		cout << endl << "-- Starting decoding --" << endl;
 	}
 
 	for(vector<bitset<HAMMING_7> >::iterator i = bitsetVector.begin(); i != bitsetVector.end();++i)
@@ -172,7 +222,11 @@ vector<bitset<N> > HammingDecoding(vector<bitset<HAMMING_7> > & bitsetVector)
 
 		// verify syndrom
 		int errorPosition = verifyBitset(inBuffer);
-		cout << endl << "position :" << errorPosition << endl;
+		if(errorPosition != 0) {
+			// there is an error, let's deal with
+			inBuffer[errorPosition-1] = inBuffer[errorPosition-1] ^ 1; // inverse symbol
+			cout << "Error corrected in position : " << errorPosition << endl;
+		}
 		
 		// transfert bit to outBuffer		
 		// right bit (poids faible - LSB)
@@ -182,21 +236,23 @@ vector<bitset<N> > HammingDecoding(vector<bitset<HAMMING_7> > & bitsetVector)
 		outBuffer[3] = C7 ; // m4 = c7 = outBuffer[6]
 		// left bit (poids fort - MSB)
 		
-		if(DEBUG_HD) {
-			cout << " | " << outBuffer.to_string();
-		}
-		
 		// add sample to decodedBitset
 		decodedBitset.push_back(outBuffer);
 	}
 	
 	if(DEBUG_HD) {
-		cout << endl;
+		cout << "After decoding :" << endl;
+	
+		for(vector<bitset<N> >::iterator i = decodedBitset.begin(); i != decodedBitset.end();++i) {
+			bitset<N> buff = *i;
+			cout << " | " << buff.to_string();
+		}
+	
+		cout << endl << "-- End decoding --" << endl;
 	}
 
 	return decodedBitset;
 }
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -211,7 +267,7 @@ int main()
 	vector< bitset<HAMMING_7> > modified_data;
 
 	// Read data to encode
-	input_data = readFile("test2.txt");
+	input_data = readFile("input2.txt");
 
 	// Encode by Hamming (7,4) coding
 	encoded_data = HammingEncoding(input_data);
@@ -221,6 +277,13 @@ int main()
 
 	// Decode
 	decoded_data = HammingDecoding(modified_data);
+	
+
+	//bitset<N> mybitset(5); // 0101
+	
+
+	
+	
 }
 
 
