13e2cad2c274f8eeefb502c7eed330fe88b427c5
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-11-15 20:44:01 +0100

    Update dst and difference

--- TP4/main.cpp
+++ TP4/main.cpp
@@ -117,18 +117,34 @@ class State {
     public :
         State () {}
         State (vector< bitset<K> > in, int dist, int diff, bitset<R> name)
-            : input(in),
+            :
             distance(dist),
             difference(diff),
             state_name(name),
             is_new(true)
-            {}
+            {
+				setInput(in);
+			}
         // getter / setter
         bitset<R> getStateName() const {return state_name;}
         vector< bitset<K> > getInput() const {return input;}
+        void setInput(vector< bitset<K> > in) {
+			input.clear();
+			for (vector<bitset<K> >::iterator it = in.begin() ; it != in.end(); ++it) {
+				input.push_back(*it);
+			}
+		}
+        
         void addInputValue(int value) {
 			input.push_back(bitset<K>(value));
 		}
+		
+		int getDifference() const { return difference; }
+		void setDifference(int diff) { difference = diff; }
+		
+		int getDistance() const { return distance; }
+		void setDistance(int dist) { distance = dist; }
+		
 		bool isNew() const { return is_new; }
 		bool isNotNew() const { return ! is_new; }
 		void setNotNew() {is_new = false;}
@@ -177,15 +193,89 @@ void insertStateInList(map< unsigned long, State > & state_list, State new_state
 	state_list.insert(pair< unsigned long , State >(new_state.getStateName().to_ulong(), new_state));
 }
 
+State * getStateFromNumber(map< unsigned long, State > & state_list, bitset<R> state_name) {
+	return & state_list.at(state_name.to_ulong());
+}
+
+void updateDistanceAndDifference(State * & state)
+{
+	state->setDistance( state->getDistance() + state->getDifference() );
+	state->setDifference(-1);
+}
+
 void computeNewState(map< unsigned long, State > & state_list, State * actual_state, bitset<N> actual_encoded_entry, int entry)
 {
+	// init vars
+	bitset<N> potential_output;
+	int difference;
+	State * old_state = actual_state;
+	
 	// compute next state name
 	bitset<R> temp_state_name = actual_state->getNextStateNameByEntry(entry);
 	
 	// check if futur state already exists
 	if( state_list.count(temp_state_name.to_ulong()) == 1 ) {
-		// TODO competition entre les distances et si = lancer piece...
-		cout << "			CAS A TRAITER - " << temp_state_name << endl;
+		
+		// get state
+		actual_state = getStateFromNumber(state_list, temp_state_name);
+		
+		// if diff == -1
+		if(actual_state->getDifference() == -1) {
+			// compute potential output
+			potential_output = actual_state->getResultByEntry(entry);
+			// count distance between potential output and actual mess_tra
+			difference = (potential_output^actual_encoded_entry).count();
+			
+			// set difference
+			actual_state->setDifference(difference);
+			// normal processing
+			actual_state->addInputValue(entry);
+		} else {
+			//-- compute old dst+diff
+			int old_total_dst = actual_state->getDistance() + actual_state->getDifference();
+			
+			//--  compute new dst+diff
+			// compute potential output
+			potential_output = actual_state->getResultByEntry(entry);
+			// count distance between potential output and actual mess_tra
+			difference = (potential_output^actual_encoded_entry).count();
+			
+			int new_total_dst = old_state->getDistance() + difference;
+			
+			if(old_total_dst < new_total_dst) {
+				// nothing to do
+				// because the other path have a better total distance
+			} else {
+				// replace data (input, dist, diff)
+				actual_state->setInput(old_state->getInput()); // WARNING: pas sûr que ça copie bien :-/
+				actual_state->addInputValue(entry);
+				
+				actual_state->setDistance(old_state->getDistance());
+				
+				actual_state->setDifference(difference);
+			}
+			
+			
+		}
+		
+		// GARDER POUR LE COMPTE RENDU ?
+		
+		// récupérer le state
+		// si diff == -1
+			// traiter normal (ajouter input, calcul diff)
+		// sinon
+			// calcul l'ancien dst+diff
+			// calcul nouveau dst+diff
+			// si ancien < nouveau
+				// rien a faire
+			// sinon
+				// si égale
+					// random (ou rien ? garder vieux ?)
+				// sinon
+					// setter input
+					// setter distance
+					// setter difference
+				
 	} else {
 		// create new state
 
@@ -193,10 +283,10 @@ void computeNewState(map< unsigned long, State > & state_list, State * actual_st
 		vector< bitset<K> > temp_input = vector< bitset<K> >(actual_state->getInput());
 		
 		// compute potential output
-		bitset<N> potential_output = actual_state->getResultByEntry(entry);
+		potential_output = actual_state->getResultByEntry(entry);
 		
 		// count distance between potential output and actual mess_tra
-		int difference = (potential_output^actual_encoded_entry).count();
+		difference = (potential_output^actual_encoded_entry).count();
 		
 		State temp_state = State(temp_input, 0, difference, temp_state_name);
 		// add entry in input
@@ -205,6 +295,9 @@ void computeNewState(map< unsigned long, State > & state_list, State * actual_st
 		// insert in the list
 		insertStateInList(state_list, temp_state);
 	}
+	
+	
+	cout << "			"; actual_state->display();
 
 }
 
@@ -223,6 +316,7 @@ vector< bitset<K> > GSM_decode(vector< bitset<N> > transmitted_message)
     
     // create first state - input:"" - dist:0 - diff:-1 - statename:0000
     State first_state =  State (vector< bitset<K> >(), 0, -1, bitset<R>(0));
+    first_state.setNotNew();
     // add first state in list
     insertStateInList(state_list, first_state);
     
@@ -240,6 +334,7 @@ vector< bitset<K> > GSM_decode(vector< bitset<N> > transmitted_message)
 			
 			#ifdef DEBUG
 			cout << "	" << "----1 state ----" << endl;
+			cout << "		"; actual_state->display();
 			#endif
 			
 			if(actual_state->isNotNew()) {
@@ -258,8 +353,9 @@ vector< bitset<K> > GSM_decode(vector< bitset<N> > transmitted_message)
 		// for each state, update distance & remove "new" flag
         for (map< unsigned long, State >::iterator actual_state_it = state_list.begin() ; actual_state_it != state_list.end(); ++actual_state_it) {
 			actual_state = & actual_state_it->second;
-			// update distance
 			
+			// update distance
+			updateDistanceAndDifference(actual_state);
 			// set to normal
 			actual_state->setNotNew();
 			
@@ -277,8 +373,8 @@ vector< bitset<K> > GSM_decode(vector< bitset<N> > transmitted_message)
 
 
     /////////// TO DELETE AND MODIFY ///////////
-    for(unsigned int i=0;i<mess_tra.size();++i) {
-        mess_dec.push_back(randBitset<K>());
+    for(unsigned int i=0;i<transmitted_message.size();++i) {
+        decoded_message.push_back(randBitset<K>());
     }
     ////////////////////////////////////////////
 
