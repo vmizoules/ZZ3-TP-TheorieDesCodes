690d594139baa9fb219700b77e48c71e91dca81e
Alexandre <alexandre.barret@poste.isima.fr>
2016-11-22 16:59:27 +0100

    Chiffre + Dechiffre VIGENERE done clean

--- TP5/main_cipher.cpp
+++ TP5/main_cipher.cpp
@@ -66,9 +66,7 @@ class Vigenere
 			keyCharacter = (char) this->key[a % this->key.length()];
 			textCharacter = (char) text[a];
 			out[a] = (char) ('A' + ((textCharacter - keyCharacter +26) %26));
-			
 		} 
-		cout << out << endl;
 		return out;
 	}
 };
@@ -79,10 +77,9 @@ class Vigenere
 
 int main()
 {
-	//Vigenere cipher("MYKEY");
-	Vigenere cipher("algorythme");
-	// string original_en  = "Kerckhoffs's principle - A cryptosystem should be secure even if everything about the system, except the key, is public knowledge.";
-	string original_en  = "pythagorepythagore";
+	Vigenere cipher("MYKEY");
+	
+	string original_en  = "Kerckhoffs's principle - A cryptosystem should be secure even if everything about the system, except the key, is public knowledge.";
 	string encrypted_en = cipher.encrypt(original_en);
 	string decrypted_en = cipher.decrypt(encrypted_en);
 
@@ -90,14 +87,14 @@ int main()
 	cout << "Encrypted: " << encrypted_en << endl;
 	cout << "Decrypted: " << decrypted_en << endl;
 
-	/*string original_fr  = "Principe de Kerckhoffs - Toute la securite d'un systeme cryptographique doit reposer sur la clef et pas sur le systeme lui meme.";
+	string original_fr  = "Principe de Kerckhoffs - Toute la securite d'un systeme cryptographique doit reposer sur la clef et pas sur le systeme lui meme.";
 
 	string encrypted_fr = cipher.encrypt(original_fr);
 	string decrypted_fr = cipher.decrypt(encrypted_fr);
 
 	cout << original_fr << endl;
 	cout << "Encrypted: " << encrypted_fr << endl;
-	cout << "Decrypted: " << decrypted_fr << endl;*/
+	cout << "Decrypted: " << decrypted_fr << endl;
 
 }
 
