749d8093dc6cadfe366d6c0672cae094d6e85a14
MIZOULES Vincent <vincent.mizoules@poste.isima.fr>
2016-11-29 17:51:46 +0100

    Compute random e, prime with phi

--- TP6/main.cpp
+++ TP6/main.cpp
@@ -13,10 +13,56 @@
 
 mpz_t d,e,n;
 mpz_t M,c;
+gmp_randstate_t r_state;
+
+// compute random between min (include) and max (include)
+void setToRandom(mpz_t & rand_Num, const unsigned int min, const unsigned int max) {
+
+	// init vars 
+	unsigned long rand_min_ui = min; // random between min (include)
+	unsigned long rand_max_ui = max; // and max (include)
+	mpz_t rand_max;
+	mpz_init(rand_max);
+	// compute randmax to make rand between 0 and rand_max (not included)
+	mpz_init_set_ui(rand_max, (unsigned long)(rand_max_ui +1 -rand_min_ui ));
+
+	mpz_urandomm(rand_Num,r_state,rand_max);
+	mpz_add_ui(rand_Num, rand_Num, rand_min_ui);
+	
+	// for DEBUG
+	//gmp_printf("%Zd\n", rand_Num);
+	
+	return;
+}
+
+// check parity of a mpz number
+bool isEven(const mpz_t & num) {
+	mpz_t r;
+	mpz_init(r);
+	bool ret = true;
+	
+	// compute modulo 2
+	mpz_cdiv_r_ui(r, num, (unsigned int) 2);
+	
+	// check if rest is == 0 or not
+	if(mpz_cmp_ui(r, 0)) {
+		ret = false;
+	}
+	
+	// clear
+	mpz_clear(r);
+	
+	return ret;
+}
 
 /* Main subroutine */
 int main()
 {
+	/* Init GMP random */
+    unsigned long int seed = time(0);
+    gmp_randinit_default (r_state);
+    gmp_randseed_ui(r_state, seed);
+	
     /* Initialize the GMP integers */
     mpz_init(d);
     mpz_init(e);
@@ -89,19 +135,43 @@ int main()
 
     /*
      *  Step 3 : Get small odd integer e such that gcd(e,x) = 1.
-     */
-    mpz_init_set_str(e, "79", 0);
-    
+     */    
+    // set random e between 2 and phi-1
+    setToRandom(e, 2, mpz_get_ui(x)-1);
+    // check if e is even
+    if(isEven(e)) {
+		// change the parity
+		mpz_add_ui(e,e,(unsigned long int)1);
+	}
+	// check gcd
+    mpz_t pgcd;
+    mpz_init(pgcd);
+    mpz_gcd(pgcd, e, x);
+    // if pgcd is not equal to 1
+    while(mpz_cmp_ui(pgcd, 1) != 0) {
+		//add 2 and re-compute
+		mpz_add_ui(e, e, (unsigned long int)2);
+		
+		mpz_gcd(pgcd, e, x);
+	}
+
+	// display pgcd
+    char pgcd_str[1000];
+    mpz_get_str(pgcd_str,10,pgcd);
+    std::cout << "\t gcd(e,x) = " << pgcd_str << std::endl;
     // display e
+    // DEBUG : mpz_init_set_str(e, "79", 0);
     char e_str[1000];
     mpz_get_str(e_str,10,e);
     std::cout << "\t e = " << e_str << std::endl;
 
+
+    
+
     /*
      *  Step 4 : Calculate unique d such that ed = 1(mod x)
      */
-    //mpz_init_set_str(d, "1019", 0);
-    mpz_invert(d, e, x);
+    mpz_invert(d, e, x); // equals to 1019 if e=79 phi=3220
     
     // display d
     char d_str[1000];
@@ -132,5 +202,8 @@ int main()
     
     mpz_clear(M);
     mpz_clear(c);
+    
+    // clean rand state
+    gmp_randclear(r_state);
 }
 
